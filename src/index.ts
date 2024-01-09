28687afa24e0208fbb4b24411bdfb68ea93dbbd5
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-01-09 21:31:24 +0300

    Implement messages

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,30 @@ import {ControlsView} from './controls-view';
 import {GameOfLife} from './game';
 import {LifeMap} from './life-map';
 import {MapView} from './map-view';
+import {MessagesView} from './messages-view';
 import {SaveGameController} from './save-game-controller';
 import {onPageReady} from './utils';
 
 // const UNIVERSE_SIZE = 2n ** 64n;
-const UNIVERSE_SIZE = 200n;
+const UNIVERSE_SIZE = 1024n;
 
 onPageReady(function() {
+    const messagesView = new MessagesView();
+    messagesView.bindToErrors();
+
     const lifeMap = new LifeMap(UNIVERSE_SIZE, UNIVERSE_SIZE);
     const mapView = new MapView(lifeMap);
     const game = new GameOfLife(lifeMap);
     const saveGameController = new SaveGameController(lifeMap);
-    const controlsView = new ControlsView(game, saveGameController);
+    const controlsView = new ControlsView(game, saveGameController, messagesView);
 
     game.onStart(mapView.endInput);
     game.onStop(mapView.beginInput);
-    game.onRound(mapView.render);
+    game.onRound(mapView.renderWhenFrame);
 
     mapView.render();
     mapView.beginInput();
     controlsView.init();
 
-    console.log('Success!');
+    console.log('Game is ready!');
 });
