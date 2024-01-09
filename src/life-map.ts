14897cb209dbfe7ead887dec28bb7a78b90f3b11
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-01-09 17:06:02 +0300

    Implement LifeMap

--- src/life-map.ts
+++ src/life-map.ts
@@ -1,7 +1,6 @@
-import type {BigIntSrc} from './common-types';
-import {ensureBigInt, enterValueToInterval} from './utils';
-
-type ChangeListener = () => void;
+import type {O} from 'ts-toolbelt';
+import type {BigIntSrc, CanString, SimpleCallback} from './common-types';
+import {call, CustomError, ensureBigInt, enterValueToInterval, obj} from './utils';
 
 export interface PopulatedRect {
     top: bigint;
@@ -10,18 +9,26 @@ export interface PopulatedRect {
     left: bigint;
 }
 
+type CoordVector = Record<string, boolean | undefined>;
+type CoordMatrix = Record<string, CoordVector>;
+
+export class LifeMapError extends CustomError {}
+
 export class LifeMap {
     public static readonly POPULATED_DELTA = 30n;
 
+    private _container: CoordMatrix;
     private _width: bigint;
     private _height: bigint;
     private _minX: bigint;
     private _maxX: bigint;
     private _minY: bigint;
     private _maxY: bigint;
-    private _changeListeners: ChangeListener[];
+    private _changeListeners: SimpleCallback[];
 
     constructor(mapWidth: BigIntSrc, mapHeight: BigIntSrc) {
+        this._container = obj() as CoordMatrix;
+
         this._width = ensureBigInt(mapWidth);
         this._height = ensureBigInt(mapHeight);
 
@@ -49,18 +56,120 @@ export class LifeMap {
         };
     }
 
+    get container(): O.Readonly<CoordMatrix> {
+        return this._container;
+    }
+
     isAlive(x: BigIntSrc, y: BigIntSrc, status?: boolean) {
         const bigX = enterValueToInterval(x, this._width);
         const bigY = enterValueToInterval(y, this._height);
 
         if (status !== undefined) {
-            // this._setStatusToContainer(bigX, bigY, status);
+            this._setStatusToContainer(bigX, bigY, status);
         }
 
         const keyX = bigX.toString();
         const keyY = bigY.toString();
 
-        console.log(keyX, keyY);
-        // return Boolean(this._container[keyX]?.[keyY]);
+        return Boolean(this._container[keyX]?.[keyY]);
+    }
+
+    addChangeListener(listener: SimpleCallback) {
+        if (this._changeListeners.indexOf(listener) === -1) {
+            this._changeListeners.push(listener);
+        }
+    }
+
+    serialize(): string {
+        const data: CanString[] = [this._width, this._height, this._minX, this._maxX, this._minY, this._maxY];
+
+        const coords: string[] = [];
+        for (const [keyX, vector] of Object.entries(this._container)) {
+            coords.push(`${keyX}:${Object.keys(vector).join(',')}`);
+        }
+        data.push(coords.join('|'));
+
+        return data.join(';');
+    }
+
+    loadSerializedState(dump: string) {
+        const data = dump.split(';');
+        if (data.length < 7) {
+            throw new LifeMapError('Invalid save data length');
+        }
+
+        this._width = BigInt(data[0]);
+        this._height = BigInt(data[1]);
+        this._minX = BigInt(data[2]);
+        this._maxX = BigInt(data[3]);
+        this._minY = BigInt(data[4]);
+        this._maxY = BigInt(data[5]);
+
+        const container = obj() as CoordMatrix;
+        for (const coordData of data[6].split('|')) {
+            const [keyX, yStr] = coordData.split(':');
+            container[keyX] = obj() as CoordVector;
+
+            for (const keyY of yStr.split(',')) {
+                container[keyX][keyY] = true;
+            }
+        }
+        this._container = container;
+
+        this._changeListeners.forEach(call);
+    }
+
+    private _setStatusToContainer(bigX: bigint, bigY: bigint, status: boolean) {
+        if (bigX >= this._width || bigX < 0n || bigY >= this._height || bigY < 0n) {
+            return;
+        }
+
+        const keyX = bigX.toString();
+        const keyY = bigY.toString();
+
+        if (status) {
+            if (this._container[keyX] === undefined) {
+                this._container[keyX] = obj() as CoordVector;
+            }
+            this._container[keyX][keyY] = true;
+        } else if (!status && this._container[keyX]) {
+            delete this._container[keyX][keyY];
+            if (Object.keys(this._container[keyX]).length === 0) {
+                delete this._container[keyX];
+            }
+        }
+
+        // TODO: Кажется, это вообще не нужно
+        if (!status) {
+            return;
+        }
+
+        if (bigX - LifeMap.POPULATED_DELTA < this._minX) {
+            this._minX = bigX - LifeMap.POPULATED_DELTA;
+            if (this._minX < 0n) {
+                this._minX = 0n;
+            }
+        }
+
+        if (bigX + LifeMap.POPULATED_DELTA > this._maxX) {
+            this._maxX = bigX + LifeMap.POPULATED_DELTA;
+            if (this._maxX >= this._width) {
+                this._maxX = this._width - 1n;
+            }
+        }
+
+        if (bigY - LifeMap.POPULATED_DELTA < this._minY) {
+            this._minY = bigY - LifeMap.POPULATED_DELTA;
+            if (this._minY < 0n) {
+                this._minY = 0n;
+            }
+        }
+
+        if (bigY + LifeMap.POPULATED_DELTA >= this._maxY) {
+            this._maxY = bigY + LifeMap.POPULATED_DELTA;
+            if (this._maxY >= this._height) {
+                this._maxY = this._height - 1n;
+            }
+        }
     }
 }
