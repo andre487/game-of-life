a56cdf83a758fda98eede4a1001485c126b3ee64
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-01-09 21:04:24 +0300

    Implement saves

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {ControlsView} from './controls-view';
 import {GameOfLife} from './game';
 import {LifeMap} from './life-map';
 import {MapView} from './map-view';
+import {SaveGameController} from './save-game-controller';
 import {onPageReady} from './utils';
 
 // const UNIVERSE_SIZE = 2n ** 64n;
@@ -11,7 +12,8 @@ onPageReady(function() {
     const lifeMap = new LifeMap(UNIVERSE_SIZE, UNIVERSE_SIZE);
     const mapView = new MapView(lifeMap);
     const game = new GameOfLife(lifeMap);
-    const controlsView = new ControlsView(game);
+    const saveGameController = new SaveGameController(lifeMap);
+    const controlsView = new ControlsView(game, saveGameController);
 
     game.onStart(mapView.endInput);
     game.onStop(mapView.beginInput);
